b29f85c6a609c018174d0893d3a25656fb001db8
AZK-GEMNET <olawole2005@gmail.com>
2022-09-11 12:58:45 +0000

    low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  * main-Entry point
  *
- * Return-Always 0 (success)
+ * Return:Always 0 (success)
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
 
