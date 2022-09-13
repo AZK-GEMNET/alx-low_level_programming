d6096ba3f5e2011d7bbe52fe490929e1ab624331
AZK-GEMNET <olawole2005@gmail.com>
2022-09-13 01:50:34 +0000

    variables

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,11 +6,12 @@
  */
 int main(void)
 {
+	char alp[26] = "zyxwvutsrqponmlkjihgfedcba";
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i = 0; i < 26; i++)
 	{
-		putchar(i);
+		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
