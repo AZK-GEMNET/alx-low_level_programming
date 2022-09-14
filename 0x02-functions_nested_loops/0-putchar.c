f236302d8a10934717ef8bdde54e771d100a65bd
AZK-GEMNET <olawole2005@gmail.com>
2022-09-14 13:36:37 +0000

    loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints putchar
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	putchar("_");
+	putchar("p");
+	putchar("u");
+	putchar("u");
+	putchar("t");
+	putchar("c");
+	putchar("a");
+	putchar("r");
+	return (0);
+}
